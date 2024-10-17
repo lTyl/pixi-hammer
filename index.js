0028c7bff71e4d2595c2b45589960fa691e12f03
Tyler Deren <contact@tderen.com>
2024-10-17 14:17:22 -0600

    Add comments

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const PIXIMath = require(`@pixi/math`);
 const PIXIEvents = require(`@pixi/events`);
 
 /**
- * @description
- * a instance helper to connect hammer to pixijs instances
- * @contructor
+ * Connects A PIXI JS V5, V6 or V7 App with HammerJS for gesture handling
+ * @param appView {HTMLCanvasElement} The canvas element that you want gestures for
+ * @param interactionManager {EventSystem|InteractionManager} A PIXI V5 or V6 InteractionManager OR a PIXI V7 EventSystem
+ * @param preInitedHammer {Hammer.Manager} An instance of a pre-initialized Hammer.Manager. Leave null for a new one to be created
+ * @param rootDisplayObject {DisplayObject} Required for PIXI V7. A DisplayObject that represents the root EventBoundary for interaction detection
+ * @constructor
  */
 var Connector = function(appView, interactionManager, preInitedHammer, rootDisplayObject) {
 	var self = this;
@@ -30,28 +33,25 @@ var Connector = function(appView, interactionManager, preInitedHammer, rootDispl
 		self._mc = new Hammer.Manager(canvas);
 	}
 };
-
 /**
  * Just prefix everything hammer-y with hammer- to avoid conflicts
  * @param  {[string]} eventName
  * @return {[string]}
  */
 function decorateEvent(eventName){
-	return "hammer-"+eventName;
+	return `hammer-${eventName}`;
 }
-
 /**
- * @description
- * get hammer manager
+ * Gets the managed Hammer Manager instance
+ * @returns {Hammer.Manager}
  */
 Connector.prototype.getManager = function() {
 	var self = this;
 	return self._mc;
 };
-
 /**
- * @description
- *
+ * Connects a list of HammerJS events to PIXI. When a tracked HammerJS event is emitted, it will propagate the event to the PIXI display object that was hit
+ * @param typesArray {string[]}
  */
 Connector.prototype.registerHandlerTypes = function(typesArray) {
 	var self = this;
@@ -67,7 +67,6 @@ Connector.prototype.registerHandlerTypes = function(typesArray) {
 	});
 
 	typesArray.forEach(function(type){
-
 		self._mc.on(type, function(evt){
 			const pixiTarget = self.config.useOnlyFirstHitTest ? firstTarget : self.getPixiTarget(evt.center);
 
@@ -75,13 +74,10 @@ Connector.prototype.registerHandlerTypes = function(typesArray) {
 				self.dispatchPixiEvent(pixiTarget, decorateEvent(type), evt);
 			}
 		});
-
 	});
 };
-
 /**
- * @description
- * destroy connector instance
+ * Removes all listeners and clears objects.
  */
 Connector.prototype.destroy = function() {
 	var self = this;
@@ -94,13 +90,16 @@ Connector.prototype.destroy = function() {
 	self._mc = null;
 	self._listeners = {};
 };
-
+/**
+ * Maps a position from the HTMLCanvasElement to a PIXI.Point that corresponds to that location on the Scene Graph
+ * @param dstPoint
+ * @returns {Point}
+ */
 Connector.prototype.normalizePoint = function(dstPoint) {
 	var pt = new PIXIMath.Point();
 	this.interactionManager.mapPositionToPoint(pt, dstPoint.x, dstPoint.y);
 	return pt;
 }
-
 /**
  * @description
  * recache the canvas bound. call this when the canvas size changed
@@ -114,12 +113,21 @@ Connector.prototype.updateCache = function(canvas) {
 		y: bound.top
 	};
 }
-
+/**
+ * Attempts to retrieve a DisplayObject from the SceneGraph that was hit by a gesture
+ * @param center {PIXI.Point} The center point of the Interaction
+ * @returns {DisplayObject|null}
+ */
 Connector.prototype.getPixiTarget = function(center){
 	var newCenter = this.normalizePoint(center);
 	return this.getHitDisplayObject(newCenter);
 }
-
+/**
+ * Perform a `hitTest` on the Scene Graph and return the first DisplayObject hit by the interaction event
+ * Works for a V5 and V6 InteractionManager and a V7 EventSystem
+ * @param center {PIXI.Point}
+ * @returns {DisplayObject|null}
+ */
 Connector.prototype.getHitDisplayObject = function(center) {
 	if (!this.isLegacyInteraction()) {
 		const boundary = new PIXIEvents.EventBoundary(this.rootDisplayObject);
@@ -128,7 +136,15 @@ Connector.prototype.getHitDisplayObject = function(center) {
 
 	return this.interactionManager.hitTest(center);
 }
-
+/**
+ * Dispatches the hammer-* event on the DisplayObject when it is interacted with
+ * When used with PIXI V5 or V6, the event will be dispatched using the `InteractionManager.dispatchEvent()` method
+ * When used with PIXI V7, the event will be dispatched via the DisplayObject's EventEmitter `emit`.
+ * This is because PIXI V7 dispatchEvent method disallows any event that does not conform to its FederatedEvent class
+ * @param pixiDisplayObject {DisplayObject} The DisplayObject that was hit by this Interaction
+ * @param eventName {string} The hammer-* event name that represents the gesture
+ * @param event {Event} The HammerJS event containing gesture data
+ */
 Connector.prototype.dispatchPixiEvent = function(pixiDisplayObject, eventName, event) {
 	if (!this.isLegacyInteraction()) {
 		pixiDisplayObject.emit(decorateEvent(event.type), event);
@@ -137,7 +153,13 @@ Connector.prototype.dispatchPixiEvent = function(pixiDisplayObject, eventName, e
 
 	this.interactionManager.dispatchEvent(pixiDisplayObject, eventName, event);
 }
-
+/**
+ * Checks if the current tracked interactionManager field is a EventSystem (PIXI V7) or a InteractionManager (V5 and V6)
+ * The InteractionManager in V7 does not have a dispatchEvent method, so we use that to determine capability
+ * If interactionManager.dispatchEvent exists, then we are running PIXI V5 or V6
+ * If it does _not_ exist, then we are running PIXI V7.
+ * @returns {boolean}
+ */
 Connector.prototype.isLegacyInteraction = function() {
 	// @pixi/events doesn't have a dispatchEvent method (that lives on the EventBoundary), so we check for that to determine capability
 	return !!this.interactionManager.dispatchEvent;
